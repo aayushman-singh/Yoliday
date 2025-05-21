b3e956fb97aaf517b1a3b94462de6493fe76fe48
Aayushman Singh <aayushman2702@gmail.com>
2025-05-21 23:10:14 +0530

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,8 +4,6 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["localhost", "picsum.photos"], // strips ports for domain approval
   },
-  basePath: "/yoliday"
-  
 };
 
 export default nextConfig;
